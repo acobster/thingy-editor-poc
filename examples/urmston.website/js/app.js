10f0415153e0b9921d7f24064ba33d339e791adc
Coby Tamayo <acobster@gmail.com>
2019-10-21 06:58:18 -0700

    refine selectors for demo 👓

--- examples/urmston.website/js/app.js
+++ examples/urmston.website/js/app.js
@@ -1,7 +1,7 @@
 thingyEditable([{
-  selector: 'li',
+  selector: '.list li',
   disallowDefaultEnter: true,
-  nested: ['a', 'a h3', 'em', 'p'],
+  nested: ['a', 'a h3', 'div em', 'p'],
   cloneOnCtrlEnter: true,
 }])
 
