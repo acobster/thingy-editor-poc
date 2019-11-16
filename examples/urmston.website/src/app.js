d873fe493f46631993c1e81e069ad23a8751448a
Coby Tamayo <acobster@gmail.com>
2019-11-15 22:17:17 -0800

    restructure

--- examples/urmston.website/src/app.js
+++ examples/urmston.website/src/app.js
@@ -1,7 +1,11 @@
+console.log('appp')
+
+
+
+// Set up our editor
 thingyEditable([{
   selector: '.list li',
   disallowDefaultEnter: true,
   nested: ['a', 'a h3', 'div em', 'p'],
   cloneOnCtrlEnter: true,
 }])
-
