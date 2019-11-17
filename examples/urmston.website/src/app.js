55a204d1c4e4385cff0899ce07eaa9921690eb01
Coby Tamayo <acobster@gmail.com>
2019-11-17 13:50:34 -0800

    make global demo site elements dynamic

--- examples/urmston.website/src/app.js
+++ examples/urmston.website/src/app.js
@@ -2,6 +2,24 @@ import { thingyEditable, localStorageBackend } from './vendor/editor.js'
 
 const data = localStorageBackend.get('app')
 
+class TextComponent extends HTMLElement {
+  constructor() {
+    super()
+    if (this.getAttribute('path')) {
+      const text = this.getAttribute('path').split('.').reduce((val, step) => {
+        if (val[step]) {
+          val = val[step]
+        } else {
+          val = ''
+        }
+
+        return val
+      }, data)
+      this.innerText = text
+    }
+  }
+}
+
 class SocialNav extends HTMLElement {
   constructor() {
     super()
@@ -23,6 +41,8 @@ class SocialNav extends HTMLElement {
   }
 }
 
+
+customElements.define('text-component', TextComponent)
 customElements.define('social-nav', SocialNav)
 
 
